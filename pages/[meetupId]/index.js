4db067967ddde26768e4cf30bfc9664062790d99
Yash Kansara <82724814+yash06112002@users.noreply.github.com>
2023-05-15 12:23:48 +0530

    fallback changed to true

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -26,7 +26,7 @@ export async function getStaticPaths() {
     const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
     client.close();
     return {
-        fallback: false,
+        fallback: true,
         paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
     }
 }
